b1c34c4d8dce17564786460f54f49b42399a0d4d
tiago <tigolimab223@gmail.com>
2024-07-12 00:06:12 -0300

    refactor: header to oop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,21 @@
+import { Component } from 'react'
 import mango from './../assets/mango.png'
-import { Switch } from './Switch'
+import Switch from './Switch'
 
-export const Header = () => {
-    return(
-        <div className='container-Header'>
-            <div className='logo'>
-                <img src={mango} alt='Logo'/>
-                <h1>
-                    Traveling Mango
-                </h1>
+class Header extends Component {
+    render() {
+        return(
+            <div className='container-Header'>
+                <div className='logo'>
+                    <img src={mango} alt='Logo'/>
+                    <h1>
+                        Traveling Mango
+                    </h1>
+                </div>
+                <Switch theme={''} />
             </div>
-            <Switch />
-        </div>
-    )
+        )
+    }
 }
+
+export default Header;
