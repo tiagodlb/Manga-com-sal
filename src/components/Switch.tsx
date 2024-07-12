0c2e7f8323e5bd9218f1ecd61827cd440ba2b023
tiago <tigolimab223@gmail.com>
2024-07-12 00:06:29 -0300

    refactor: switch to oop

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,26 +1,48 @@
-import { useEffect, useState } from "react"
+import { Component, useEffect, useState } from "react"
 
 type Theme = 'dark' | 'light'
 type ChangeEvent = React.ChangeEvent<HTMLInputElement>
-
-export const Switch = () => {
-    const [theme, setTheme] = useState<Theme>('light')
-
-    const handleChange = (e: ChangeEvent) => setTheme(e.target.checked ? 'dark' : 'light');
-
-    useEffect(() => {
-
-        document.body.setAttribute('data-theme', theme);
-
-    }, [theme]);
-    
-    return (
-        <div className="container-switch">
-            <span>Mude o Tema</span>
-            <label className="switch">
-                <input type="checkbox" onChange={handleChange} checked={theme === 'dark'} />
-                <span className="slider"></span>
-            </label>
-        </div>
-    )
+type MyProps = {
+    theme: string,
 }
+
+type MyState = {
+    theme: string,
+}
+
+
+
+class Switch extends Component<MyProps, MyState> {
+    constructor(props: MyProps) {
+        super(props);
+        this.state = {
+            theme: 'dark'
+        };
+    }
+
+    handleChange = (e: ChangeEvent): void => {
+        if(e.target.checked) {
+            this.setState({theme: 'dark' })
+        } else {
+            this.setState({theme: 'light'});
+        }
+    }
+
+    componentDidUpdate(): void {
+        document.body.setAttribute('data-theme', this.state.theme);
+    }
+
+    render() {
+        return (
+            <div className="container-switch">
+                <span>Mude o Tema</span>
+                <label className="switch">
+                    <input type="checkbox" onChange={this.handleChange} checked={this.state.theme === 'dark'} />
+                    <span className="slider"></span>
+                </label>
+            </div>
+        )
+    }
+}
+
+export default Switch
