d7905effd27542b14834b4d3ed8e900dcd634f77
tiago <tigolimab223@gmail.com>
2024-07-12 00:06:56 -0300

    refactor: oop

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { LayoutCards } from './components/Card'
-import { Header } from './components/Header'
+import LayoutCards from './components/Card'
+import Header from './components/Header'
 
 function App() {
-  return (
-    <>
-    <Header />
-    <div className="container-main">
-      <LayoutCards />
-    </div>
-    </>
-  )
+      return (
+      <>
+        <Header />
+        <div className="container-main">
+          <LayoutCards />
+        </div>
+      </>
+    )
 }
 
 export default App
